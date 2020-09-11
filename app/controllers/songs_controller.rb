8ed7d8c5768eb622f14a764c222c90da6e9252b9
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-09-11 10:16:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,25 +1,34 @@
 class SongsController < ApplicationController
 
   def index
-  end 
-  
-  def show 
-  end 
-  
-  def new 
-  end 
-  
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
   def create
-  end 
-  
+    @song = Song.create(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
   def update
-  end 
-  
-  def edit 
-  end 
-  
-  private 
-  
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  private
+
   def song_params(*args)
     params.require(:song).permit(*args)
   end
