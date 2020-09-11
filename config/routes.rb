558a6d724cbfd40759a92a8e40201307f26c7750
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-09-11 10:05:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :artists, only: [:index, :show, :new, :create, :update, :edit]
+  resources :artists, only: [:artists, :show, :new, :create, :update, :edit]
+  resources :genres, only: [:show, :new, :create, :update, :edit]
+  resources :songs, only: [:index, :show, :new, :create, :update, :edit]
   
 end
