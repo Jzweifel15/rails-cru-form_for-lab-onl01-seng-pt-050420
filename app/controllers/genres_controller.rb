eced22b096e453d7b399dfdc319078ee4fd89ab6
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-09-11 10:08:59 +0000

    Automatically backed up by Learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,27 @@
+class GenresController < ApplicationController
+
+  def index
+  end 
+  
+  def show 
+  end 
+  
+  def new 
+  end 
+  
+  def create
+  end 
+  
+  def update
+  end 
+  
+  def edit 
+  end 
+  
+  private 
+  
+  def _params(*args)
+    params.require(:model).permit(*args)
+  end
+
+end
