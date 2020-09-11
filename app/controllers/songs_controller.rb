fc09341ea45e45b8e52361a8b8c63dd9029d0232
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-09-11 10:09:11 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+
+  def index
+  end 
+  
+  def show 
+  end 
+  
+  def new 
+  end 
+  
+  def create
+  end 
+  
+  def update
+  end 
+  
+  def edit 
+  end 
+  
+  private 
+  
+  def _params(*args)
+    params.require(:model).permit(*args)
+  end
+
+end
